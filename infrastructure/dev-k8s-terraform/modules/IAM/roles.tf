363f10fb6e1817304e9df9ea0c80a421f4683816
Akgün D <101358551+AkgunD@users.noreply.github.com>
2022-10-12 16:10:23 +0300

    Update roles.tf
    fixed master-worker name

--- infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
+++ infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
@@ -69,12 +69,12 @@ resource "aws_iam_policy_attachment" "attach_for_worker" {
 }
 
 resource "aws_iam_instance_profile" "profile_for_master" {
-  name  = var.profile_for_master1
+  name  = var.profile_for_master
   role = aws_iam_role.role_for_master.name
 }
 
 resource "aws_iam_instance_profile" "profile_for_worker" {
-  name  = var.profile_for_worker1
+  name  = var.profile_for_worker
   role = aws_iam_role.role_for_worker.name
 }
 
